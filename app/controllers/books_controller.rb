121bff57d954a067b96f893d030dff64c0b2014f
EC2 Default User <ec2-user@ip-172-31-92-105.ec2.internal>
2022-05-08 09:58:29 +0000

    [Update]3-task

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,22 +1,22 @@
 class BooksController < ApplicationController
-  def new
-    @book=Book.new
-  end
 
   def create
     book=Book.new(book_params)
     book.save
-    redirect_to '/books/edit'
+    redirect_to book_path(book.id)
   end
 
   def index
     @books=Book.all
+    @book=Book.new
   end
 
   def show
+    @book=Book.find(params[:id])
   end
 
   def edit
+
   end
 
   private
--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,5 @@
 Rails.application.routes.draw do
   root to: 'homes#top#'
-
-  get 'books/new'
-  post 'books'=>"books#create"
-  get '/books'=>"books#index"
-  get 'books/show'
-  get 'books/edit'
-
+  resources:books
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
